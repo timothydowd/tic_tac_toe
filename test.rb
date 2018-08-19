d41b0d0e841d9fed81e292590bd51ea825e3de1f
dwdy250 <timdowd1984@gmail.com>
2018-08-19 20:52:19 +0100

    array fixed

--- test.rb
+++ test.rb
@@ -1,17 +1,37 @@
 
-nest_array = [[123],[456],[789]]
+
 
 def find_cell(choice)
-  nest_array.map do |row|
+  nest_array = [[123],[456],[789]]
+  nest_array.map! do |row|
     row.map do |cell|
       if cell == choice
         cell = "X"
+      else
+        cell
       end
     end
   end
+  puts nest_array
 end
 
 
 find_cell(7)
+puts "\n"
 
-puts nest_array
+def find_cell2(choice)
+  nest_array = [[123],[456],[789]]
+  nest_array.map! do |row|
+    row.map do |cell|
+      if cell == choice
+        cell = "X"
+      else
+        cell
+      end
+    end
+  end
+  puts nest_array
+end
+
+
+find_cell2(1)
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -36,9 +36,9 @@ class Game
 
   def show_board
 
-    puts board.board_array[0].join(" ")
-    puts board.board_array[1].join(" ")
-    puts board.board_array[2].join(" ")
+    puts board.board_array[0..2].join(" ")
+    puts board.board_array[3..5].join(" ")
+    puts board.board_array[6..8].join(" ")
     puts "\n"
   end
 
@@ -52,6 +52,7 @@ class Game
     self.take_turn
   end
 
+
   def take_turn
     self.show_board
 
@@ -61,50 +62,28 @@ class Game
       player1.chosen_cells << player1.current_choice
       self.place_piece
 
-      #board.board_array.map! {|cell|
-        #if cell == player2.current_choice
-        #    cell = player2.piece
-      #  end
-    #  }
     else
       puts "#{@player2.name}, choose a square:"
       player2.current_choice = gets.chomp
       player2.chosen_cells << player1.current_choice
       self.place_piece
-
-      #board.board_array.map! {|cell|
-        #if cell == player2.current_choice
-          #  cell = player2.piece
-        #end
-    #  }
     end
-    #board.board_array
-    self.show_board
 
-
+    puts "\n"    
   end
+
 
   def place_piece
     if @player1.turn
-      board.board_array.map do |row|
-        row.map do |cell|
-          if cell == player1.current_choice.to_i
-            cell = player1.piece
-          end
-        end
+      board.board_array.map! do |cell|
+        cell == player1.current_choice.to_i ? cell = player1.piece : cell
       end
 
     else
-
-      board.board_array.map do |row|
-        row.map do |cell|
-          if cell == player2.current_choice.to_i
-            cell = player2.piece
-          end
-        end
+      board.board_array.map! do |cell|
+        cell == player2.current_choice.to_i ? cell = player2.piece : cell
       end
     end
-
   end
 
 
@@ -130,7 +109,7 @@ end
 class Board
 
   def initialize
-    @board_array = [[1,2,3],[4,5,6],[7,8,9]]
+    @board_array = [1,2,3,4,5,6,7,8,9]
   end
 
   def board_array
