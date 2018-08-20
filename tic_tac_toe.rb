b9256c0e1939dfdcdd8df9b963eeb826471155c5
dwdy250 <timdowd1984@gmail.com>
2018-08-20 13:38:12 +0100

    started work with check_combos method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,8 @@
 
 class Game
 
+  WIN_COMBOS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
   def player1 # getter
     @player1
   end
@@ -11,14 +13,20 @@ class Game
     @board
   end
 
+  def win_combos
+    WIN_COMBOS
+  end
+
+
   #-----------------------------------------------------------
 
   def initialize
     self.create_players
     self.game_start
+
   end
 
-  def create_players
+  def create_players # creates players, takes names and sets O or X
     @player1 = Player.new
     @player2 = Player.new
 
@@ -34,7 +42,7 @@ class Game
 
   end
 
-  def show_board
+  def show_board #shows board
 
     puts board.board_array[0..2].join(" ")
     puts board.board_array[3..5].join(" ")
@@ -43,47 +51,61 @@ class Game
   end
 
 
-
-
   def game_start # creates board and chooses first player to start
     @board = Board.new
-
-    rand <= 0.5 ? @player1.turn = true : @player2.turn = true
+    @player1.turn = true #remember to delete this code
+    #rand <= 0.5 ? @player1.turn = true : @player2.turn = true
     self.take_turn
   end
 
 
-  def take_turn
+  def take_turn # records player cell choice
     self.show_board
 
     if @player1.turn
       puts "#{@player1.name}, choose a square:"
-      player1.current_choice = gets.chomp
+      player1.current_choice = gets.chomp.to_i
       player1.chosen_cells << player1.current_choice
       self.place_piece
-
     else
       puts "#{@player2.name}, choose a square:"
-      player2.current_choice = gets.chomp
+      player2.current_choice = gets.chomp.to_i
       player2.chosen_cells << player1.current_choice
       self.place_piece
     end
+    puts "\n"
 
-    puts "\n"    
+    self.check_combos
   end
 
 
-  def place_piece
+  def place_piece # updates board after player chooses cell
     if @player1.turn
       board.board_array.map! do |cell|
-        cell == player1.current_choice.to_i ? cell = player1.piece : cell
+        cell == player1.current_choice ? cell = player1.piece : cell
       end
-
     else
       board.board_array.map! do |cell|
-        cell == player2.current_choice.to_i ? cell = player2.piece : cell
+        cell == player2.current_choice ? cell = player2.piece : cell
       end
     end
+  end
+
+  def check_combos # checks to see if we have a winning combination
+    puts "in check_combos"
+    puts "player 1 chosen cells = #{player1.chosen_cells}"
+    self.win_combos.each do |combo|
+      combo.each do |cell|
+        if player1.chosen_cells.any? { |chosen| chosen == cell}
+          
+          puts "match #{combo} #{cell}"
+          puts "\n"
+        else
+          puts "no match"
+        end
+      end
+    end
+
   end
 
 
